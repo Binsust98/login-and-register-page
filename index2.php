746f79d6b49db2ceaa7168afabe02b8bf0066afc
binsust98 <binsust98@gmail.com>
2020-12-01 13:40:15 +0530

    login page

---
+++ index2.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>student details</title>
+    <h1>Sign up</h1>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+<body>
+    <table class="table">
+        <tr>
+            <tr>
+                <td>FIRST NAME</td>
+                <td><input class="form-control"type="text" name="" id=""> </td>
+                <td>LAST NAME</td>
+                <td><input class="form-control"type="text" name="" id=""></td>
+            </tr>
+            <tr>
+                <td>AGE</td>
+                <td><input class="form-control"type="text" name="" id=""> </td>
+                <td>DOB</td>
+                <td><input class="form-control"type="text" name="" id=""></td>
+            </tr>
+            <tr>
+                <td>MOBILE NO</td>
+                <td><input class="form-control"type="text" name="" id=""> </td>
+                <td>EMAIL ID</td>
+                <td><input class="form-control"type="text" name="" id=""></td>
+            </tr>
+            <tr>
+                <td>PLACE</td>
+                <td><input class="form-control"type="text" name="" id=""> </td>
+                <td>USER NAME</td>
+                <td><input class="form-control"type="text" name="" id=""></td>
+            </tr>
+            <tr>
+                <td>PASSWORD</td>
+                <td><input class="form-control"type="text" name="" id=""> </td>
+                <td>CONFIRM PASSWORD</td>
+                <td><input class="form-control"type="text" name="" id=""></td>
+            </tr>
+            <tr>
+                <tr>
+                  
+                  
+                    
+                       <tr>
+                           <td>male</td>
+                           <td><input type="radio" name="Male" id=""></td>
+
+                               <td>Female</td>
+                               <td><input type="radio" name="Female" id=""></td>
+                        <tr>
+                           
+                        </tr>
+                       </tr>
+                    </td>
+                </tr>
+            </tr>
+           <tr>
+               <tr>
+                  
+                  
+               </tr>
+               
+               <tr>
+                   
+               </tr>
+           </tr>
+ 
+        </tr>
+    </table>
+    <tr>
+        <tr>
+            <td>DECLARATION:   I agree that the above information is correct and complete to the best of my knowledge and belief.
+                  </td>
+            <td></td>
+            
+            
+            </tr>
+        </tr>
+        <table>
+            <tr>
+                <t></t> 
+                <td></td>
+                <td><input type="checkbox" name="" id=""></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td>
+               </td>
+               <td></td>
+
+               <td>I Agree </td>
+            
+        </tr>
+            <tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td><td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td><td></td>
+                    <td></td><td></td>
+                    <td></td><td></td>
+                    <td></td><td></td>
+                    <td></td><td></td>
+                    <td></td><td></td>
+                    <td></td><td></td>
+                    <td></td><td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><button class="btn btn-danger">REGISTER</button></td>
+                    <td></td>
+                </tr>
+            </tr>
+        </table>
+    </tr>
+    
+    <tr>
+        <tr>
+           
+    </tr>
+    
+ 
+  
+ 
+  
+   
+</html>
